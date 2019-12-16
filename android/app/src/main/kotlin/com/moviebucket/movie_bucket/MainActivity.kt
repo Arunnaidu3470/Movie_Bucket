c9aa509f025a0395b31a94349b7b12bc50f0613f
Ratakondala Arun <arunratakondala3470@gmail.com>
2019-12-16 19:28:59 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/moviebucket/movie_bucket/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.moviebucket.movie_bucket
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
