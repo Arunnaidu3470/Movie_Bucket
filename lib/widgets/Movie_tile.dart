3670ed3da53d168aab954e4901843ad52c180811
Ratakondala Arun <arunratakondala3470@gmail.com>
2019-12-21 16:48:08 +0530

    added new widget -movietile(optmized)

---
+++ lib/widgets/Movie_tile.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:movie_bucket/constants/constants.dart';
+import 'package:movie_bucket/pages/Movie_details.dart';
+import 'package:movie_bucket/services/api_services.dart';
+
+class MovieTile extends StatelessWidget {
+  final List<dynamic> movieList;
+  final Widget preWidget;
+  MovieTile({this.movieList, this.preWidget});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      child: _assemble(),
+    );
+  }
+
+  Widget _assemble() {
+    int addPreTile = 0;
+    if (preWidget != null) addPreTile = 1;
+    return ListView.builder(
+      physics: BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal,
+      itemCount: movieList.length + addPreTile,
+      itemBuilder: (context, index) {
+        if (addPreTile != 0 && index == 0) return _pre();
+        return _Tile(
+          imagePath: movieList[index - addPreTile][MovieConstants.MOVIE_POSTER],
+          movieTitle: movieList[index - addPreTile][MovieConstants.MOVIE_TITLE],
+          movieId: movieList[index - addPreTile][MovieConstants.MOVIE_ID],
+        );
+      },
+    );
+  }
+
+  Widget _pre() {
+    return FittedBox(
+        fit: BoxFit.cover,
+        child: Container(
+            height: 200, width: 120, child: Center(child: preWidget)));
+  }
+}
+
+class _Tile extends StatelessWidget {
+  final String imagePath;
+  final String movieTitle;
+  final int movieId;
+  final double tileHeight = 200, tileWidth = 120;
+  _Tile({this.imagePath, this.movieTitle, this.movieId});
+
+  _nextPage(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+      return MovieDetailsPage(
+        id: movieId,
+        title: movieTitle,
+      );
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _nextPage(context),
+      child: Container(
+        height: (tileHeight),
+        width: (tileWidth),
+        margin: EdgeInsets.symmetric(horizontal: 2),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _image(),
+            _title(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _image() {
+    return SizedBox(
+      height: (tileHeight - 30.0),
+      width: (tileWidth),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        elevation: 10,
+        child: ClipRRect(
+          clipBehavior: Clip.antiAlias,
+          borderRadius: BorderRadius.circular(8),
+          child: (imagePath != null)
+              ? Image.network(
+                  ImageServices.getImageUrlOf(imagePath,
+                      size: ImageServices.POSTER_SIZE_MEDIUM),
+                  fit: BoxFit.cover,
+                )
+              : Icon(Icons.local_movies),
+        ),
+      ),
+    );
+  }
+
+  Widget _title() {
+    return SizedBox(
+      width: tileHeight - 40,
+      height: (tileHeight - (tileHeight - 20)),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 5),
+        child: Text(
+          movieTitle,
+          style: TextStyle(fontSize: 12),
+          overflow: TextOverflow.ellipsis,
+        ),
+      ),
+    );
+  }
+}
